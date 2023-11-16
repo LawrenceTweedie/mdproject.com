a8407aa0ee049e0917481558ead66756c0f18457
StormGate98 <150802903+StormGate98@users.noreply.github.com>
2023-11-16 19:28:00 +0500

    dashboard

---
+++ dashboard/predicts.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>User Dashboard</title>
+</head>
+<body>
+<header>
+<center>
+ <a href="index.php"><img src="/../assets/images/i8.png" alt="Логотип" width="325" height="150"></a>
+</center>
+  </header>
+  </header>
+<main>
+<nav class="navbar navbar-expand-sm navbar-light">
+	  <div class="container-fluid ">
+		<a class="navbar-brand " href="/../index.php">Главная</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+        <div class="container-fluid ">
+		<a class="navbar-brand " href="/../login.php">Профиль</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+        <a class="navbar-brand " href="predicts.php">Прогнозы</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+    <a class="navbar-brand " href="index.php">Статистика</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+	  </div>
+	</nav>
+</main>
+<body>
+<br></br>
+<center><h5 class="display-6">Прогнозы</h5></center>
+<br></br>
+<div class="container text-center">
+<div id="passwordHelpBlock" class="form-text">
+  best of 3
+</div>
+  <div class="row align-items-center">
+    <div class="col">
+    <img src="/../assets/images/teams/6134.png" alt="9pandas" width="50" height="50"> 
+    <text>9Pandas</text>
+    </div>
+    <div class="col">
+    <div class="progress">
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 37%" aria-valuenow="37" aria-valuemin="0" aria-valuemax="100">37%</div>
+  <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 63%" aria-valuenow="63" aria-valuemin="0" aria-valuemax="100">63%</div>
+</div>
+    </div>
+    <div class="col">
+    <text>OG</text>
+    <img src="/../assets/images/teams/44.png" alt="OG" width="50" height="50">
+    </div>
+  </div>
+  <div id="passwordHelpBlock" class="form-text">
+17.11.2023 / 20:00
+</div>
+  <br></br>
+  <div id="passwordHelpBlock" class="form-text">
+  best of 1
+</div>
+  <div class="row align-items-center">
+    <div class="col">
+    <img src="/../assets/images/teams/5778.png" alt="WinterBear" width="50" height="50"> 
+    <text>Winter Bear</text>
+    </div>
+    <div class="col">
+    <div class="progress">
+  <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 53%" aria-valuenow="53" aria-valuemin="0" aria-valuemax="100">53%</div>
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 47%" aria-valuenow="47" aria-valuemin="0" aria-valuemax="100">47%</div>
+</div>
+    </div>
+    <div class="col">
+    <text>Virtus.Pro</text>
+    <img src="/../assets/images/teams/72.png" alt="VP" width="50" height="50">
+    </div>
+  </div>
+  <div id="passwordHelpBlock" class="form-text">
+18.11.2023 / 19:00
+</div>
+<br></br>
+  <div id="passwordHelpBlock" class="form-text">
+  best of 1
+</div>
+  <div class="row align-items-center">
+    <div class="col">
+    <img src="/../assets/images/teams/25.png" alt="Navi" width="50" height="50"> 
+    <text>Na'Vi</text>
+    </div>
+    <div class="col">
+    <div class="progress">
+  <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 79%" aria-valuenow="79" aria-valuemin="0" aria-valuemax="100">79%</div>
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 21%" aria-valuenow="21" aria-valuemin="0" aria-valuemax="100">21%</div>
+</div>
+    </div>
+    <div class="col">
+    <text>Virtus.Pro</text>
+    <img src="/../assets/images/teams/72.png" alt="OG" width="50" height="50">
+    </div>
+  </div>
+  <div id="passwordHelpBlock" class="form-text">
+18.11.2023 / 20:30
+</div>
+<br></br>
+  <div id="passwordHelpBlock" class="form-text">
+  best of 1
+</div>
+  <div class="row align-items-center">
+    <div class="col">
+    <img src="/../assets/images/teams/48.png" alt="LGD" width="50" height="50"> 
+    <text>LGD Gaming</text>
+    </div>
+    <div class="col">
+    <div class="progress">
+  <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100">66%</div>
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 34%" aria-valuenow="34" aria-valuemin="0" aria-valuemax="100">34%</div>
+</div>
+    </div>
+    <div class="col">
+    <text>Talon Esports</text>
+    <img src="/../assets/images/teams/5726.png" alt="OG" width="50" height="50">
+    </div>
+  </div>
+  <div id="passwordHelpBlock" class="form-text">
+18.11.2023 / 22:00
+</div>
+</div>
+</body>
+</html>
